0dd22db13b2c474f2bc0d74af4e7cb43968a3b22
Eric Pascual <eric@pobot.org>
2016-03-19 11:54:14 +0100

    added scripts installation

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
 
 import textwrap
 
-setup(name='pybot-dynamixel',
-      namespace_packages=['pybot'],
-      version='1.0',
-      description='Dynamixel servos control',
-      install_requires=['pybot-core'],
-      license='LGPL',
-      long_description=textwrap.dedent("""
+setup(
+    name='pybot-dynamixel',
+    namespace_packages=['pybot'],
+    version='1.0',
+    description='Dynamixel servos control',
+    install_requires=['pybot-core'],
+    license='LGPL',
+    long_description=textwrap.dedent("""
             This sub-package contains classes allowing controlling
             Dynamixel servos.
       """),
-      author='Eric Pascual',
-      author_email='eric@pobot.org',
-      url='http://www.pobot.org',
-      download_url='https://github.com/Pobot/PyBot',
-      packages=find_packages("src"),
-      package_dir={'': 'src'}
-      )
+    author='Eric Pascual',
+    author_email='eric@pobot.org',
+    url='http://www.pobot.org',
+    download_url='https://github.com/Pobot/PyBot',
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'dmxl-setid = pybot.dynamixel.cli.dmxl_setid:main',
+            'dmxl-scan = pybot.dynamixel.cli.dmxl_scan:main',
+            'dmxl-regs = pybot.dynamixel.cli.dmxl_regs:main'
+        ]
+    }
+)
